8e326d5f89d62785db0b3e06fb500d6235e8557e
mardocheo-source <mardocheo.mf@gmail.com>
2025-04-17 10:57:17 +0900

    updated forecats for japan and alert

--- welcome.js
+++ welcome.js
@@ -95,6 +95,11 @@ const directoryStructure = {
                 "aggregated_mag_BalancedAverage.pdf": null
             }
         },
+        "2025-April-May": {
+            "1 day resolution": {
+                "Japan-2025-04-13__05-13.pdf": null
+            }
+        },
         "2025-Feb-2025-Dec": {
             "1 day resolution": {
                 "Japan-2025-03-11-30.pdf": null
